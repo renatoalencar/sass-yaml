b248a15116f3c4e0616cb44ef0d361657ee8a668
Renato Alencar <renatoalencar.73@gmail.com>
2017-05-23 14:59:42 -0300

    add suport to more sass types

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var types = require('node-sass').types;
 var YAML = require('yamljs');
 var path = require('path');
@@ -6,11 +8,11 @@ function readExtendedObjectKey(keys, data) {
   var value = data;
 
   for (var key of keys.split('.')) {
-    switch (typeof data) {
-      case 'Object':
+    switch (typeof value) {
+      case 'object':
         value = value[key];
         break;
-      case 'Array'
+      case 'array':
         value = value[parseInt(key)];
         break;
     }
@@ -19,17 +21,41 @@ function readExtendedObjectKey(keys, data) {
   return value
 }
 
+function toSassObject(value) {
+
+
+  return {
+    string: types.String,
+    object: function (item) {
+      if (value === null)
+        return types.Null();
+
+      var value = types.Map(item.length);
+
+      for (var i = 0; i < item.length; i++) {
+        var key = item.keys()[i];
+        value.setKey(i, key);
+        value.setValue(i, item[key]);
+      }
+
+      return value
+    },
+    number: types.Number,
+    boolean: types.Boolean
+  }[typeof value](value)
+}
+
 module.exports = function (options) {
   options = options || {};
 
-  var base = options.base || process.pwd();
+  var base = options.base || process.cwd();
 
   return {
     'yaml($item, $file)': function (item, file) {
       var filename = path.resolve(base, '.', file.getValue());
-      var data = readExtendedObjectKey(item, YAML.load(filename));
+      var data = readExtendedObjectKey(item.getValue(), YAML.load(filename));
 
-      return types.String(data);
+      return toSassObject(data);
     }
   }
 }
